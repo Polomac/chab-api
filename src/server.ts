4f480413cbe1d537e69c8a877908ec8e00dd507c
Milan Polomac <milanpolomac@Milans-MacBook-Air.local>
2025-08-21 17:14:27 +0200

    Register chat user

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import express, {Request, Response }from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import { StreamChat } from 'stream-chat';
 
 dotenv.config();
 
@@ -9,6 +10,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+const chatClient = StreamChat.getInstance(process.env.STREAM_KEY!, process.env.STREAM_SECRET!);
 
 app.post('/register', async (req: Request, res: Response): Promise<any> => {
 
@@ -24,9 +27,35 @@ app.post('/register', async (req: Request, res: Response): Promise<any> => {
       error: 'Username and password are required',
     });
   }
-  res.status(200).json({
-    message: 'User registered successfully',
-  });
+  
+  const uid = email.replace(/[^a-zA-Z0-9_-]/g, '_');
+
+  try {
+    console.log(uid)
+    const userResponse = await chatClient.queryUsers({id: {$eq: uid}});
+
+    if (!userResponse.users.length) {
+      await chatClient.upsertUser({
+        id: uid,
+        name: name,
+        //@ts-ignore
+        email: email,
+        role: 'user',
+      });
+    } else {
+      return res.status(400).json({
+        error: 'User already exists',
+      });
+    }
+
+    return res.status(200).json({ message: 'User registered successfully' });
+  
+  } catch (error) {
+    console.error('Error registering user:', error);
+    return res.status(500).json({
+      error: 'Internal server error',
+    });
+  }
 });
 
 const PORT = process.env.PORT || 6000;
